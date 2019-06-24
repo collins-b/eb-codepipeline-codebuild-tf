bd570e8036b183d01fbb8916c167efe2c044edf8
Collins Wekesa <collinsdelan@gmail.com>
2019-06-24 17:38:18 +0300

    update

--- build.tf
+++ build.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "artifacts" {
 }
 
 resource "aws_codebuild_project" "build" {
-  name = "incredible-website-project"
+  name = "tf-eb-project"
   description = "Builds for the project"
   build_timeout = "5"
   service_role = "${aws_iam_role.build.arn}"
